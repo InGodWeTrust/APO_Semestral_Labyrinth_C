5506e5aeb686c535cfda71286a1f14ab08ec847f
Tima <timaliharev@gmail.com>
2017-05-14 16:24:00 +0200

    change image loading and unit adding

--- game_unit.c
+++ game_unit.c
@@ -2,14 +2,11 @@
 
 
 void add_unit_to_screen(uint16_t *screen_matrix, uint16_t *unit_matrix, int x_pos, int y_pos) {
-	int i, j;
-	
-	for (i = 0; i < MAIN_UNIT_HEIGHT; i++) {
-		for (j = 0; j < 30; j++, unit_matrix++) {
-			*(screen_matrix + i*SCREEN_WIDTH + j) = 0x0000;
-		}
-		for (j = 30; j < 40; j++, unit_matrix++) {
-			*(screen_matrix + i*SCREEN_WIDTH + j) = 0xffff;
-		}
-	}
+    int i, j;
+
+    for (i = x_pos; i < MAIN_UNIT_HEIGHT; i++) {
+        for (j = y_pos; j < 30; j++, unit_matrix++) {
+            *(screen_matrix + i * SCREEN_WIDTH + j) = *unit_matrix;
+        }
+    }
 }
--- game_utils.c
+++ game_utils.c
@@ -5,36 +5,41 @@ typedef struct {
     unsigned char green;
     unsigned char red;
 } Rgb;
-   
+
 
 uint16_t rgb_to_16(char r, char g, char b) {
 
-	uint16_t rgb16 = 0;
-	rgb16 |= (r * 31 / 255) << 11;
-	rgb16 |= (g * 63 / 255) << 5;
-	rgb16 |= (b * 31 / 255); 
+    uint16_t rgb16 = 0;
+    rgb16 |= (r * 31 / 255) << 11;
+    rgb16 |= (g * 63 / 255) << 5;
+    rgb16 |= (b * 31 / 255);
 
-	return rgb16;
+    return rgb16;
 }
 
-uint16_t *load_image(const char* img_addr, int width, int height) {
-	
-	int i, j;
-    Rgb rgb;
-	uint16_t *result = (uint16_t *) malloc(sizeof(uint16_t) * height * width);
-	FILE *inFile = fopen(img_addr, "rb");
-	
-	fseek(inFile, 54, SEEK_SET);
-    
-    for (i = 0; i < height; i++) {
-		for (j = 0; j < width; j++, result++) {
-			fread(&rgb, sizeof(Rgb), 1, inFile);
-			printf("%i %i %i \n", rgb.red, rgb.green, rgb.blue);
-			*result = rgb_to_16(rgb.red, rgb.green, rgb.blue);
-		}
-	}
-    
+uint16_t *load_image(const char *img_addr, int width, int height) {
 
-	fclose(inFile);
-	return result - height * width;
+    int i, j;
+    Rgb rgb;
+    uint16_t *result = (uint16_t *) malloc(sizeof(uint16_t) * height * width);
+    FILE *inFile = fopen(img_addr, "rb");
+
+    fseek(inFile, 54, SEEK_SET);
+
+    for (i = 0; i < height; i++) {
+        for (j = 0; j < width; j++) {
+            fread(&rgb, sizeof(Rgb), 1, inFile);
+            printf("%i %i %i \n", rgb.red, rgb.green, rgb.blue);
+
+            //Invert image
+            int index = i * width + j;
+            int new_index = index % width + (height - index/height - 1) * width;
+
+            result[index] = rgb_to_16(rgb.red, rgb.green, rgb.blue);
+        }
+    }
+
+
+    fclose(inFile);
+    return result - height * width;
 }
