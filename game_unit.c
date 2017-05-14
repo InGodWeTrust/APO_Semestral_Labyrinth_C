f04adda703c84cc066f54878d00244af7a355a43
Tima <timaliharev@gmail.com>
2017-05-14 16:25:45 +0200

    unit adding changed

--- game_unit.c
+++ game_unit.c
@@ -4,8 +4,8 @@
 void add_unit_to_screen(uint16_t *screen_matrix, uint16_t *unit_matrix, int x_pos, int y_pos) {
     int i, j;
 
-    for (i = x_pos; i < MAIN_UNIT_HEIGHT; i++) {
-        for (j = y_pos; j < 30; j++, unit_matrix++) {
+    for (i = y_pos; i < MAIN_UNIT_HEIGHT; i++) {
+        for (j = x_pos; j < MAIN_UNIT_WIDTH; j++, unit_matrix++) {
             *(screen_matrix + i * SCREEN_WIDTH + j) = *unit_matrix;
         }
     }
