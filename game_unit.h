70804f30e5d819df5593879fe636ce9e47afa829
Tima <timaliharev@gmail.com>
2017-05-14 14:00:35 +0200

    change game_unit

--- game_unit.h
+++ game_unit.h
@@ -2,16 +2,15 @@
 #define GAME_UNIT_H
 
 typedef struct movable_unit {
-	unit_orientation orientation,
-	unit_type type,
-	int x_pos,
-	int y_pos
-}
-
+	unit_orientation orientation;
+	unit_type type;
+	int x_pos;
+	int y_pos;
+};
 typedef enum unit_type {
 	MAIN_UNIT = 0,
 	HUNTER_UNIT = 1
-}
+};
 
 typedef enum unit_orientation {
 	LEFT 		= 	0,
@@ -21,7 +20,7 @@ typedef enum unit_orientation {
 	RIGHT 		= 	3,
 	RIGHT_DOWN 	= 	4,
 	RIGHT_UP 	= 	5
-}
+};
 
 #endif
 
